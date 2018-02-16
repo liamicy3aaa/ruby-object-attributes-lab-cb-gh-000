76ed8667903907ae7dca74504ce4a4cd7278b1a3
Liam McClelland <mcclelland-109773@3aaa.ac.uk>
2018-02-16 14:23:53 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -4,6 +4,15 @@ class Dog
     @dog_name = dog_name
   end
   
+  def breed=(dog_breed)
+    @dog_breed = dog_breed
+  end
+  
   def name
     @dog_name
   end
+  
+  def breed
+    @dog_breed
+  end
+end
